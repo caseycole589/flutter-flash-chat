a43e8dcd3e6364c3ad2c4f33a48b8cbee8af3fb9
casey cole <casey@capsinc.com>
2021-06-14 16:58:42 -0400

    added animation for logo

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -16,10 +16,13 @@ class _LoginScreenState extends State<LoginScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Container(
-              height: 200.0,
-              child: Image.asset('images/logo.png'),
+          children: [
+            Hero(
+              tag: 'logo',
+              child: Container(
+                height: 200.0,
+                child: Image.asset('images/logo.png'),
+              ),
             ),
             SizedBox(
               height: 48.0,
